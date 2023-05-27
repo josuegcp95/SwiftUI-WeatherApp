a926ca817ec5f9de95a536abb1708c2d089799c2
Josue <josuegcp95@gmail.com>
2023-05-27 13:01:39 -0400

    Initial commit

---
+++ SwiftUI-WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Josue Cruz on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-WeatherApp/WeatherButton.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherButton.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Josue Cruz on 5/17/23.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor.gradient)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
+
+
